82ef920630c7f272a140e505c266a7475cdd4d10
Marcio <marciopsj@yahoo.com.br>
2022-07-20 20:53:13 -0300

    background body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
+import Bg1 from "../assets/bg1.png";
+import Bg2 from "../assets/bg2.png";
+
+
 const globalStyle = createGlobalStyle`
     * {
         padding: 0;
@@ -12,8 +16,11 @@ const globalStyle = createGlobalStyle`
     }
 
     body {
+        background: url(${Bg2}) no-repeat 1010px, url(${Bg1}) no-repeat 931px;
+        background-size: cover;
         background-color: #F8FDFF;
         font-family: 'Poppins', sans-serif;
+
     }
 
     button {
